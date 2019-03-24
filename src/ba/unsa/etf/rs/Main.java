88153dc273959927af23f364c65cbde195f1aa17
Kerim246 <mbektasevi1@etf.unsa.ba>
2019-03-25 00:05:23 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rs/Main.java
@@ -0,0 +1,59 @@
+package ba.unsa.etf.rs;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static double funkcija(double []niz){
+        double max;
+        max=niz[0];
+        for(int i=0 ; i<niz.length ; i++){
+            if(niz[i]>max)
+                max=niz[i];
+        }
+
+        return max;
+    }
+
+    public static double korijen(double []niz,double X){
+        double korijen;
+        int brojac=0;
+
+        double kor = Math.sqrt(X);
+
+        for(int i=0 ; i<niz.length ; i++ ){
+            if(niz[i] > kor){
+                brojac = brojac + 1;
+            }
+        }
+        return brojac;
+    }
+
+    public static void main(String[] args) {
+        Scanner ulaz = new Scanner(System.in);
+
+        System.out.println("Unesite velicinu niza: ");
+
+        int n;
+
+        do {
+             n = ulaz.nextInt();
+        } while (n<0 || n>100);
+
+        double niz[] = new double[n];
+
+        System.out.println("Unesite clanove niza: ");
+
+        for(int i=0 ; i<niz.length ; i++){
+            niz[i] = ulaz.nextInt();
+        }
+
+        double max = funkcija(niz);
+
+        System.out.println("Broj članova niza koji su veći od korijena najvećeg člana niza iznosi: "+korijen(niz,max));
+
+
+    }
+
+}
+
